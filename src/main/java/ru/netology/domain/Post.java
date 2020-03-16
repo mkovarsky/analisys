549c061fd2c7a858c2a319e49cd8b029d5ecd7a8
mkovarky <mikhail.kovarsky@gmail.com>
2020-03-16 18:34:32 +0300

    add get/set

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,6 +1,54 @@
 package ru.netology.domain;
 
 public class Post {
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getPostImageUrl() {
+        return postImageUrl;
+    }
+
+    public void setPostImageUrl(String postImageUrl) {
+        this.postImageUrl = postImageUrl;
+    }
+
+    public String getPostAuthor() {
+        return postAuthor;
+    }
+
+    public void setPostAuthor(String postAuthor) {
+        this.postAuthor = postAuthor;
+    }
+
+    public String getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(String postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    public PostInfo getPostInfo() {
+        return postInfo;
+    }
+
+    public void setPostInfo(PostInfo postInfo) {
+        this.postInfo = postInfo;
+    }
+
     private String postId;
     private String postImageUrl;
     private String postAuthor;
