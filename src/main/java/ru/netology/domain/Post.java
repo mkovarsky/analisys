8a5591f2c5a4eac439e5ee569ddbe3cc4322791e
mkovarky <mikhail.kovarsky@gmail.com>
2020-03-16 17:42:38 +0300

    add classes

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+public class Post {
+    private String postId;
+    private String postImageUrl;
+    private String postAuthor;
+    private String postDate;
+    private String postContent;
+    private PostInfo postInfo;
+
+}
---
+++ src/main/java/ru/netology/domain/PostInfo.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+public class PostInfo {
+    private int likeCount;
+    private AddLike addLike;
+    private int commentCount;
+    private AddComment addComment;
+    private int shareCount;
+    private SharePost sharePost;
+    private int viewCount;
+}
