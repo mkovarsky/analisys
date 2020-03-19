2799f96ce668dadb18d87eff7193f377283a9758
mkovarky <mikhail.kovarsky@gmail.com>
2020-03-19 14:53:42 +0300

    add PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    Post post = new Post();
+
+    public Post[] search(int ownerId) {
+        return null;
+    }
+
+    public void delete(int Id, int ownerId) {
+    }
+
+}
